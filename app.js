667b9ed3737e8b9d9e396774c5553a212d045d21
ElisianeQuadros <elisiane.quadros@hotmail.com>
2024-09-18 17:35:42 -0300

    Updating

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
-
+app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
